22a90137f163685421e6922e24057530c3a53a8e
Quyen Ho <46450315+HOMINHQUYEN-1996-TLUS@users.noreply.github.com>
2022-03-26 18:38:39 +0700

    update database

--- database.sql
+++ database.sql
@@ -26,3 +26,48 @@ select username,password from manager_login
 
 select sdt from customer_login
 
+CREATE TABLE Bacsi(
+	ID_bacsi char(20) NOT NULL PRIMARY KEY,
+	HoTen nvarchar(100),
+	Birthday char(30),
+	GioiTinh char(10),
+	DiaChi NVARCHAR (100)
+)
+
+CREATE TABLE khachhang(
+	ID_khachhang char(20) NOT NULL PRIMARY KEY,
+	HoTen nvarchar(100),
+	Birthday char(30),
+	GioiTinh char(10),
+	DiaChi NVARCHAR (100)
+)
+
+GO
+
+CREATE TABLE PhongBan (
+	ID_PhongBan char(20) NOT NULL PRIMARY KEY,
+	TenPhong NVARCHAR(100),
+	ID_bacsi char(20) FOREIGN KEY (ID_bacsi) REFERENCES Bacsi(ID_bacsi),
+	ID_khachhang char(20) FOREIGN KEY (ID_khachhang) REFERENCES khachhang(ID_khachhang)
+)
+
+GO 
+
+CREATE table HoaDon(
+	ID_HoaDon char(20) NOT NULL PRIMARY KEY,
+	ID_bacsi char(20) FOREIGN KEY (ID_bacsi) REFERENCES Bacsi(ID_bacsi),
+	ID_PhongBan char(20) FOREIGN KEY (ID_PhongBan) REFERENCES PhongBan(ID_PhongBan),
+	NgayThang char(100),
+	TongTien char(20)
+)
+
+GO
+
+CREATE TABLE DichVu(
+	ID_DichVu char(20) NOT NULL PRIMARY KEY,
+	TenDichVu NVARCHAR(200),
+	GiaDichVu char(20)
+)
+
+ALTER TABLE PhongBan
+  ADD ID_DichVu char(20) FOREIGN KEY (ID_DichVu) REFERENCES DichVu(ID_DichVu);
